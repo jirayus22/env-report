dc26f9d88b7b942fdaad62ffe583b4e11b3123ff
jirayus <x-mark42@hotmail.com>
2026-04-26 23:59:03 +0700

    fix : pdf on usermanul page

--- app/user-manual/page.tsx
+++ app/user-manual/page.tsx
@@ -1,17 +1,14 @@
 export default function UserManual() {
   return (
-    <div className="pt-5" style={{ height: "100vh", overflow: "hidden" }}>
-      <iframe
-        src="https://phdb.moph.go.th/main/upload/web_news_files/yb3uzzbb3r4w44okk.pdf"
-        width="100%"
-        height="100%"
-        style={{
-          border: "none",
-          overflow: "auto",
-          WebkitOverflowScrolling: "touch",
-        }}
-        scrolling="yes"
-      />
+    <div className="pt-5" style={{ height: "100vh" }}>
+      <div style={{ height: "100%", overflow: "auto" }}>
+        <iframe
+          src="https://phdb.moph.go.th/main/upload/web_news_files/yb3uzzbb3r4w44okk.pdf"
+          width="100%"
+          height="100%"
+          style={{ border: "none" }}
+        />
+      </div>
     </div>
   );
 }
