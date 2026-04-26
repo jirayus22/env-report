8700fe75269247cf592480af4a1ab7f9754354f5
jirayus <x-mark42@hotmail.com>
2026-04-27 00:01:50 +0700

    fix : pdf on usermanul page

--- app/user-manual/page.tsx
+++ app/user-manual/page.tsx
@@ -3,7 +3,7 @@ export default function UserManual() {
     <div className="pt-5" style={{ height: "100vh" }}>
       <div style={{ height: "100%", overflow: "auto" }}>
         <iframe
-          src="https://phdb.moph.go.th/main/upload/web_news_files/yb3uzzbb3r4w44okk.pdf"
+          src="https://docs.google.com/gview?embedded=true&url=https://phdb.moph.go.th/main/upload/web_news_files/yb3uzzbb3r4w44okk.pdf"
           width="100%"
           height="100%"
           style={{ border: "none" }}
